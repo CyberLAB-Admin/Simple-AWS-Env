58d8975302739646daf5800a2a96d785a4691835
CyberLAB-Admin <hayden.loader@nextgen.group>
2024-11-27 16:14:32 +1100

    cleaned up variables.tf and main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,3 @@
-# Variables
-variable "project_prefix" {
-  description = "Prefix for all resources"
-  type        = string
-}
-
-variable "mongodb_password" {
-  description = "MongoDB admin password"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "aws_account_id" {
-  description = "AWS Account ID"
-  type        = string
-}
-
 # Provider configuration
 provider "aws" {
   region = var.aws_region
