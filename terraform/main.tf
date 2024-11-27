f3c3bc2b585ba873fd485395764e5fc735abbc1d
CyberLAB-Admin <hayden.loader@nextgen.group>
2024-11-27 17:45:17 +1100

    tasky debug

--- terraform/main.tf
+++ terraform/main.tf
@@ -273,6 +273,17 @@ module "eks" {
   }
 }
 
+# Kubernetes Provider Configuration
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
 # Outputs
 output "mongodb_ip" {
   value = aws_instance.mongodb.public_ip
